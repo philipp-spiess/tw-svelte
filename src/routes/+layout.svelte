9f7d92f98b96da83eda3c1caf65da1e6e53238e8
Philipp Spiess <hello@philippspiess.com>
2024-12-20 11:52:16 +0100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import "../app.css";
+  let { children } = $props();
+</script>
+
+<div class="p-2">
+  {@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<h1 class="font-bold">Welcome to SvelteKit</h1>
+<div class="foo">
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</div>
+
+<style>
+  @import "../app.css" reference;
+
+  .foo {
+    @apply underline text-blue-500;
+  }
+</style>
